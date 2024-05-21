88054e998fee5603cf79800794cb6ec1dde61e5c
Mukesh Kumar Patel <mukeshkumarpatel142@gmail.com>
2024-05-21 12:59:32 +0530

    Add project files.

---
+++ TasteCart.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,12 @@
+﻿namespace TasteCart.Services.CouponAPI.Models
+{
+    public class Coupon
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get;}
+        public double DiscountAmount { get; set; }
+        public int MinAmount {  get; set; }
+       // public DateTime LastUpdated { get; set; }
+
+    }
+}
