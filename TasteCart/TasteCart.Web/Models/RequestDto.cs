d2821b209f1323a28dd37519fa2a85e747871056
Mukesh Kumar Patel <mukeshkumarpatel142@gmail.com>
2024-06-15 23:09:27 +0530

    Till BaseService

---
+++ TasteCart/TasteCart.Web/Models/RequestDto.cs
@@ -0,0 +1,12 @@
+﻿using static TasteCart.Web.Utility.SD;
+
+namespace TasteCart.Web.Models
+{
+    public class RequestDto
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string ApiUrl { get; set; }
+        public object Data {  get; set; }
+        public string AccessToken {  get; set; }
+    }
+}
---
+++ TasteCart/TasteCart.Web/Service/IService/IBaseService.cs
@@ -0,0 +1,9 @@
+﻿using TasteCart.Web.Models;
+
+namespace TasteCart.Web.Service.IService
+{
+    public interface IBaseService
+    {
+        Task<ResponseDto?>SendAsync(RequestDto requestDto);
+    }
+}
